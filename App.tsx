515339a1c5bdf7a1d270a6848da10df38374d1b0
Danil Shashkov <shashkovdanil@gmail.com>
2020-07-05 17:07:28 +0500

    Format

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  console.log(1)
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
